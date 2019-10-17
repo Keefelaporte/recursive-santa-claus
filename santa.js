08296faf405783a257c9a9db94e776754309c944
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-10-16 21:08:04 -0400

    santa

--- santa.js
+++ santa.js
@@ -1,5 +1,10 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 0) return
+  
+  var first = houses [0]
+  var rest = houses.slice(1, houses.length)
+  console.log('Delivering presents to ' + first)
+  deliverPresents(rest)
 }
 
 module.exports = deliverPresents
